94da08d698b16cb3356ab800ecc52f032dd4fdfb
DhruvArvindSingh <dhruvsinghxd@gmail.com>
2025-07-09 03:05:02 +0530

    feat: Added footerList

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,6 +10,9 @@ import {
   IonTitle,
   IonToolbar,
   isPlatform,
+  IonGrid,
+  IonRow,
+  IonCol,
 } from "@ionic/react";
 import { APP_NAME, DATA } from "../app-data";
 import * as AppGeneral from "../components/socialcalc/index.js";
@@ -37,46 +40,46 @@ const Home: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [authLoading, setAuthLoading] = useState(false);
   const [currentFilePassword, setCurrentFilePassword] = useState<string | null>(null);
-  // const getDeviceType = () => {
-  //   // Use Ionic's isPlatform for more reliable detection
-  //   if (isPlatform("android")) {
-  //     console.log("android from isPlatform");
-  //     return "Android";
-  //   }
-  //   if (isPlatform("ipad")) {
-  //     console.log("ipad from isPlatform");
-  //     return "iPad";
-  //   }
-  //   if (isPlatform("iphone")) {
-  //     console.log("iphone from isPlatform");
-  //     return "iPhone";
-  //   }
-  //   // Ionic's isPlatform does not support "ipod", so check user agent for iPod
-  //   if (/iPod/.test(navigator.userAgent)) {
-  //     console.log("ipod from user agent");
-  //     return "iPod";
-  //   }
+  const getDeviceType = () => {
+    // Use Ionic's isPlatform for more reliable detection
+    if (isPlatform("android")) {
+      console.log("android from isPlatform");
+      return "Android";
+    }
+    if (isPlatform("ipad")) {
+      console.log("ipad from isPlatform");
+      return "iPad";
+    }
+    if (isPlatform("iphone")) {
+      console.log("iphone from isPlatform");
+      return "iPhone";
+    }
+    // Ionic's isPlatform does not support "ipod", so check user agent for iPod
+    if (/iPod/.test(navigator.userAgent)) {
+      console.log("ipod from user agent");
+      return "iPod";
+    }
 
-  //   // Fallback to user agent detection for web browsers
-  //   const ua = navigator.userAgent || navigator.vendor || (window as any).opera;
-  //   if (/android/i.test(ua)) {
-  //     console.log("android from user agent");
-  //     return "Android";
-  //   }
-  //   if (/iPad/.test(ua) && !(window as any).MSStream) {
-  //     console.log("ipad from user agent");
-  //     return "iPad";
-  //   }
-  //   if (/iPhone/.test(ua) && !(window as any).MSStream) {
-  //     console.log("iphone from user agent");
-  //     return "iPhone";
-  //   }
-  //   if (/iPod/.test(ua) && !(window as any).MSStream) {
-  //     console.log("ipod from user agent");
-  //     return "iPod";
-  //   }
-  //   return "default";
-  // };
+    //   // Fallback to user agent detection for web browsers
+    const ua = navigator.userAgent || navigator.vendor || (window as any).opera;
+    if (/android/i.test(ua)) {
+      console.log("android from user agent");
+      return "Android";
+    }
+    if (/iPad/.test(ua) && !(window as any).MSStream) {
+      console.log("ipad from user agent");
+      return "iPad";
+    }
+    if (/iPhone/.test(ua) && !(window as any).MSStream) {
+      console.log("iphone from user agent");
+      return "iPhone";
+    }
+    if (/iPod/.test(ua) && !(window as any).MSStream) {
+      console.log("ipod from user agent");
+      return "iPod";
+    }
+    return "default";
+  };
   const [device] = useState("default");
 
   const store = new Local();
@@ -90,7 +93,7 @@ const Home: React.FC = () => {
   };
 
   useEffect(() => {
-    const data = DATA["home"][device]["msc"];
+    const data = DATA["home"][getDeviceType()]["msc"];
     AppGeneral.initializeApp(JSON.stringify(data));
     checkAuthStatus();
   }, []);
@@ -131,30 +134,12 @@ const Home: React.FC = () => {
     }
   };
 
-  const footers = DATA["home"][device]["footers"];
-  const footersList = footers.map((footerArray) => {
-    return (
-      <IonButton
-        key={footerArray.index}
-        expand="full"
-        color="light"
-        className="ion-no-margin"
-        onClick={() => {
-          updateBillType(footerArray.index);
-          activateFooter(footerArray.index);
-          setShowPopover({ open: false, event: undefined });
-        }}
-      >
-        {footerArray.name}
-      </IonButton>
-    );
-  });
-
+  const footers = DATA["home"][getDeviceType()]["footers"];
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar color="primary">
-          <IonTitle>{APP_NAME}</IonTitle>
+          <IonTitle>Editing : {selectedFile}</IonTitle>
           <Login
             slot="end"
             onLoginClick={() => {
@@ -211,13 +196,30 @@ const Home: React.FC = () => {
               setShowPopover({ open: false, event: undefined })
             }
           >
-            {footersList}
           </IonPopover>
         </IonToolbar>
         <IonToolbar color="secondary">
-          <IonTitle className="ion-text-center">
-            Editing : {selectedFile}
-          </IonTitle>
+          <IonGrid>
+            <IonRow className="ion-nowrap">
+              {footers.map((footerArray) => (
+                <IonCol key={footerArray.index} size="auto">
+                  <IonButton
+                    expand="block"
+                    color="light"
+                    size="small"
+                    className="ion-no-margin"
+                    onClick={() => {
+                      updateBillType(footerArray.index);
+                      activateFooter(footerArray.index);
+                      setShowPopover({ open: false, event: undefined });
+                    }}
+                  >
+                    {footerArray.name}
+                  </IonButton>
+                </IonCol>
+              ))}
+            </IonRow>
+          </IonGrid>
         </IonToolbar>
 
         <IonFab vertical="bottom" horizontal="end" slot="fixed">
